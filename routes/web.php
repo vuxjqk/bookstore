0d86f0ad77ab111d0bb4af4e3c09e9a22f0d9675
vuxjqk <vuxjqk@gmail.com>
2025-08-04 14:18:40 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/auth/{provider}', [SocialiteController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [SocialiteController::class, 'callback']);
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware(['role:admin'])->group(function () {
+        //
+    });
+});
+
+require __DIR__ . '/auth.php';
